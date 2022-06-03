1e07da021dcd62af9915139af77af72c495f731f
Froz10 <ivan.frozby@gmail.com>
2022-06-03 17:08:58 +0600

    Refactroing error handler

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,4 +5,6 @@
 import { application } from "./application"
 
 import HeaderController from "./header_controller"
+import ErrorController from "./error_controller"
 application.register("header", HeaderController)
+application.register("error", ErrorController)
--- app/javascript/errors.js
+++
@@ -1,7 +0,0 @@
-var alert_del = document.querySelectorAll('.alert-del');
-
-alert_del.forEach((x)=> {
-    x.addEventListener('click', ()=> 
-      x.parentElement.classList.add('hidden')
-    );
-});
