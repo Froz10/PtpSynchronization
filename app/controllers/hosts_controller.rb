80ecfcca79160de3ae0b9e699817d832537fc91c
Froz10 <ivan.frozby@gmail.com>
2022-08-14 23:54:46 +0600

    Fix

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,6 +1,6 @@
 class HostsController < ApplicationController
   def index
-    
+    @hosts = Host.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :import_hosts_to_dbs, ActiveAdmin::Devise.config
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'home#index'
