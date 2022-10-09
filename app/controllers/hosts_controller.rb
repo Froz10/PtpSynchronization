4982cdf6ba1075d2ed9a4a08708a708eb8d9559d
Froz10 <ivan.frozby@gmail.com>
2022-10-10 00:12:13 +0600

    Configuring stimulus to modal form

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -4,11 +4,15 @@ class HostsController < ApplicationController
 
   def search
     if params[:hostname].present?
-      hosts = Host.filter_by_name(params[:hostname]).map(&:name)
+      @hosts = Host.filter_by_name(params[:hostname]).map(&:name)
     else
-      hosts = []
+      @hosts = []
     end
    
-    render(json: { hostnames: hosts })
+    respond_to do |format|
+      format.json {
+        render json: { hostnames: @hosts }, partial: "shared/modal", locals: { hosts: @hosts }
+      }
+    end
   end
 end
--- app/javascript/controllers/panel_controller.js
+++ app/javascript/controllers/panel_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { enter, leave, toggle } from 'el-transition'
 
 export default class extends Controller {
-  static targets = ['closeButton', 'input', 'output'];
+  static targets = ['closeButton', 'input', 'output', 'hostnameChoice'];
 
   connect() {
     this.closeButtonTarget.addEventListener('click', () => {
@@ -13,8 +13,27 @@ export default class extends Controller {
   }
 
   findHostname() {
-    fetch(`/hosts/search?hostname=${ this.inputTarget.value }`)
+    fetch(`/hosts/search?hostname=${ this.inputTarget.value }`, {
+      headers: { accept: 'application/json'} })
       .then((response) => response.json())
-      .then((data) => this.outputTarget.textContent = data.hostnames);
+      .then((data) => {
+        var hostsHTML = "";
+        var hostsArray = Object.values(data)[0]
+        hostsArray.forEach(host => {
+          hostsHTML += this.hostTemplate(host)
+        });
+        this.outputTarget.innerHTML = hostsHTML;
+    });
+  }
+
+  hostTemplate(host) {
+    return `
+      <li class="px-3 py-2 cursor-pointer hover:bg-gray-200" data-action="click->panel#hostnameChoice">
+        <h4 data-panel-target='hostnameChoice(${host})'> ${host}</h4>
+      </li>`
+  }
+
+  hostnameChoice(host) {
+    this.inputTarget.value = `${host.originalTarget.textContent.trim()}`
   }
 }
