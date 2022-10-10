d02cbc4ea8f18519128f16de0c144692577588da
Froz10 <ivan.frozby@gmail.com>
2022-10-10 18:02:57 +0600

    Tuning ptp modal

--- app/javascript/controllers/panel_controller.js
+++ app/javascript/controllers/panel_controller.js
@@ -1,8 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 import { enter, leave, toggle } from 'el-transition'
 
+var arraySize = 0
+
 export default class extends Controller {
-  static targets = ['closeButton', 'input', 'output', 'hostnameChoice'];
+  initialize() {
+    this.element.addEventListener("hidden", this.hidePanel())
+  }
+
+  static targets = ['closeButton', 'input', 'output', 'hostnameChoice', 'source'];
 
   connect() {
     this.closeButtonTarget.addEventListener('click', () => {
@@ -11,8 +17,9 @@ export default class extends Controller {
       leave(document.getElementById('modal-panel'));
     });
   }
-
+  
   findHostname() {
+    this.sourceTarget.classList.remove("hidden")
     fetch(`/hosts/search?hostname=${ this.inputTarget.value }`, {
       headers: { accept: 'application/json'} })
       .then((response) => response.json())
@@ -23,17 +30,26 @@ export default class extends Controller {
           hostsHTML += this.hostTemplate(host)
         });
         this.outputTarget.innerHTML = hostsHTML;
-    });
+        arraySize = hostsArray.length
+        this.hidePanel(arraySize)
+    })   
   }
 
   hostTemplate(host) {
-    return `
+      return `
       <li class="px-3 py-2 cursor-pointer hover:bg-gray-200" data-action="click->panel#hostnameChoice">
-        <h4 data-panel-target='hostnameChoice(${host})'> ${host}</h4>
+        <h3 data-panel-target='hostnameChoice(${host})'> ${host}</h3>
       </li>`
   }
 
   hostnameChoice(host) {
     this.inputTarget.value = `${host.originalTarget.textContent.trim()}`
+    this.sourceTarget.classList.add("hidden")
+  }
+
+  hidePanel() {
+    if ( arraySize === 0 ) {
+      this.sourceTarget.classList.add("hidden")
+    }
   }
 }
