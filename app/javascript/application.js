84a2066d0704953b360987a16f84a4009c8d572c
Froz10 <ivan.frozby@gmail.com>
2022-10-16 10:10:48 +0600

    Delete rails ujs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Entry point for the build script in your package.json
-import Rails from "@rails/ujs"
-import "./controllers"
-
-Rails.start()
 import "@hotwired/turbo-rails"
+import "./controllers"
