6c7322e8e0c09735a7bdaa0eed3dac11587ce93d
Froz10 <ivan.frozby@gmail.com>
2023-01-08 15:50:50 +0600

    fix jquery issue for active admin

--- app/javascript/active_admin.js
+++ app/javascript/active_admin.js
@@ -1,2 +1,3 @@
-import './packages/jquery.js' // position for hoisting
+import './packages/jquery.js'
+import './packages/jquery-ui.js'
 import '@activeadmin/activeadmin/app/assets/javascripts/active_admin/base.js';
