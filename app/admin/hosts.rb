bc4324822887334dfb1cee859a9202c8f30ee9fc
Froz10 <ivan.frozby@gmail.com>
2023-01-08 22:45:31 +0600

    add activeadmin addons

--- app/admin/hosts.rb
+++ app/admin/hosts.rb
@@ -45,7 +45,7 @@ ActiveAdmin.register Host do
     f.inputs do
       f.input :name
       f.input :address
-      f.input :region_id, as: :select, collection: Region.all.collect { |region| [region.name, region.id] }
+      f.input :region_id, as: :select, input_html: { class: "select2" }, collection: Region.all.collect { |region| [region.name, region.id] }
     end
     f.actions
   end
--- app/javascript/active_admin.js
+++ app/javascript/active_admin.js
@@ -1,3 +1,5 @@
 import './packages/jquery.js'
 import './packages/jquery-ui.js'
+import './packages/select2.js'
 import '@activeadmin/activeadmin/app/assets/javascripts/active_admin/base.js'
+import 'activeadmin_addons/src/all.js'
