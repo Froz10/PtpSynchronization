b633726d122f5d66dad7cabe582f2c984af29500
Froz10 <ivan.frozby@gmail.com>
2023-04-12 16:05:37 +0600

    Create pagination

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -5,6 +5,7 @@ class HostsController < ApplicationController
     @regions = Region.all
     @region = Region.find(params[:region_id])
     @q = @region.hosts.ransack(params[:q])
-    @pagy, @hosts = pagy_countless(@q.result(distinct: true), items: 8)
+    hosts = @q.result(distinct: true).order(created_at: :asc)
+    @pagy, @hosts = pagy_countless(hosts, items: 8)
   end
 end
